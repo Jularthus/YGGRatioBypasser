66deb09edad0497e985c3c526aab61954df44f82
Jules-Arthus Klein <jularthus.yt@gmail.com>
2023-05-23 14:02:08 +0200

    a

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from requests import post
 from properties import pwd
+from time import sleep
 
 reqID = 1
+tracker = "joinpeers.org"
 
 url = "http://localhost:8112/json"
 headers = {'Accept': 'application/json',
@@ -11,25 +13,47 @@ data = {"id": reqID, "method": "auth.login", "params": [pwd]}
 getAuth = post(url, headers=headers, json=data)
 headers['Cookie'] = getAuth.headers['Set-Cookie'].split(";")[0]
 
-def getTorrentsList():
+def getSpecificTorrentsList(tracker):
     global reqID
     reqID += 1
-    data = {"method":"web.update_ui","params":[["queue","name","total_wanted","state","progress","num_seeds","total_seeds","num_peers","total_peers","download_payload_rate","upload_payload_rate","eta","ratio","distributed_copies","is_auto_managed","time_added","tracker_host","download_location","last_seen_complete","total_done","total_uploaded","max_download_speed","max_upload_speed","seeds_peers_ratio","total_remaining","completed_time","time_since_transfer","label"],{"tracker_host":"tracker.wf"}],"id":reqID}
+    data = {"method":"web.update_ui","params":[["queue","name","total_wanted","state","progress","num_seeds","total_seeds","num_peers","total_peers","download_payload_rate","upload_payload_rate","eta","ratio","distributed_copies","is_auto_managed","time_added","tracker_host","download_location","last_seen_complete","total_done","total_uploaded","max_download_speed","max_upload_speed","seeds_peers_ratio","total_remaining","completed_time","time_since_transfer","label"],{"tracker_host":tracker}],"id":reqID}
+    req = post(url, headers=headers, json=data)
+    return req.json()
+
+def getAllTorrentsList():
+    global reqID
+    reqID += 1      #A FAIRE SUPER IMPORTANT
+    data = {"method":"web.update_ui","params":[["queue","name","total_wanted","state","progress","num_seeds","total_seeds","num_peers","total_peers","download_payload_rate","upload_payload_rate","eta","ratio","distributed_copies","is_auto_managed","time_added","tracker_host","download_location","last_seen_complete","total_done","total_uploaded","max_download_speed","max_upload_speed","seeds_peers_ratio","total_remaining","completed_time","time_since_transfer","label"],{"tracker_host":""}],"id":reqID}
     req = post(url, headers=headers, json=data)
     return req.json()
     
 
-def setTrackers(torrentID, name):
+def deleteTrackers(torrentID, name):
     global reqID
     reqID += 1
     data = {"method":"core.set_torrent_trackers","params":[torrentID,[]],"id":reqID}
     post(url, headers=headers, json=data)
-    print(f'Trackers supprimés pour {name} !')
+    print(f'❌ Trackers supprimés pour {name} !')
 
-def checker():
-    for i in getTorrentsList()['result']['torrents']:
-        setTrackers(i, getTorrentsList()['result']['torrents'][i]['name'])
+def addTrackers(torrentID, name):
+    global reqID
+    reqID += 1
+    data = {"method":"core.set_torrent_trackers","params":[torrentID,[{"tier":0,"url":"http://connect.joinpeers.org:8080/xJpxLN32Qy8PRlOl6hYaoEsl8uAoFk9j/announce"}]],"id":reqID}
+    post(url, headers=headers, json=data)
+    print(f'✅ Trackers ajoutés pour {name} !')
 
-while 1:
-    checker()
-    print('t')
+def auto():
+    for j in getSpecificTorrentsList(tracker)['result']['torrents']:
+        progress = getSpecificTorrentsList(tracker)['result']['torrents'][j]['progress']
+        if progress>0 and progress<100:
+               deleteTrackers(j, getSpecificTorrentsList(tracker)['result']['torrents'][j]['name'])
+    for a in getAllTorrentsList()['result']['torrents']:
+        progress = getAllTorrentsList()['result']['torrents'][a]['progress']
+        if progress==100:
+             addTrackers(a, getAllTorrentsList()['result']['torrents'][a]['name'])
+        
+while True:
+    auto()
+    sleep(5)
+
+#http://connect.joinpeers.org:8080/xJpxLN32Qy8PRlOl6hYaoEsl8uAoFk9j/announce
